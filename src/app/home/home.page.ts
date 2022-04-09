ecd33e2b519d74ba253d39074eee40a9f6e9284d
Ngiliade <naasongiliade@outlook.com>
2022-04-08 21:05:33 -0300

    Projeto concluido

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nome1 = ""
+  nome2 = ""
+  url = "http://lucasreno.kinghost.net/love-calculator/"
+  resultado = 0
+  mensagem = ""
+  calculando = false;
+  imagem = false
 
-  constructor() {}
+  constructor(
+    public http: HttpClient,
+  ) { }
+
+  async enviarDados() {
+    let soma = 0;
+    this.imagem = false
+    while (soma != 10) {
+      this.resultado = Math.floor(Math.random() * 100 + 1);
+      this.calculando = true;
+      soma += 1;
+      await this.delay(75);
+    }
+
+    
+    this.calculando = false;
+
+    this.http.get<any>(this.url + this.nome1 + "/" + this.nome2).subscribe(
+      (resposta: any) => {
+        this.resultado = resposta;
+        if (this.resultado < 20) this.mensagem = "Deu nada, sai dessa";
+        else if (this.resultado < 40) this.mensagem = "O não já tem, busca a humilhação";
+        else if (this.resultado < 60) this.mensagem = "Não tá ruim, mas também não tá bom";
+        else if (this.resultado < 80) this.mensagem = "Acho que só amizade mesmo";
+        else{ 
+          this.imagem = true;
+          this.mensagem = "Acho que só amizade mesmo";
+        }
+      }
+    );
+    if(this.nome1 == "" || this.nome2 == ""){
+      this.mensagem = "Cade os nomes?";
+      this.resultado = 0;
+    }
+    
+    
+  }
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 
 }
